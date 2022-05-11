9d9502a88468b327b054eeff697a289e0a852dab
Nickolas Carlos <nickolascarlos@gmail.com>
2022-05-11 14:05:02 -0300

    feat: Exports interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import BrazilCitiesPredictor from './predictor';
 import * as strategies from './strategies';
+import { AbsoluteRatedCity } from './interfaces/absoluteRatedCity.interface';
+import { RelativeRatedCity } from './interfaces/relativeRatedCity.interface';
+import { City } from './interfaces/city.interface';
+import { ProcessedCity } from './interfaces/processedCity.interface';
 
-export { strategies };
+export { strategies, AbsoluteRatedCity, RelativeRatedCity, City, ProcessedCity};
 export default BrazilCitiesPredictor;
